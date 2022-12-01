bc9b9615c18ec20df054201a9111643a922852de
Drew Abbott <abbotta4@gmail.com>
2022-12-01 17:00:31 -0600

    Add day 1 part 2

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -6,6 +6,10 @@ def read_input(infile):
 def find_most_calories(elves):
     return sum(sorted(elves, key=lambda elf: sum(elf))[-1])
 
+def find_top_three(elves):
+    return sum([sum(x) for x in sorted(elves, key=lambda elf: sum(elf))[-3:]])
+
 if __name__ == "__main__":
     elves = read_input("input.txt")
     print(find_most_calories(elves))
+    print(find_top_three(elves))
