77b767a1ae219f6a97c9d9da8d831ff70348febe
Drew Abbott <abbotta4@gmail.com>
2022-12-03 01:08:46 -0600

    Add day 3 part 2

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -1,3 +1,5 @@
+from more_itertools import chunked
+
 def read_input(infile):
     with open(infile, 'r') as f:
         _input = [[x[:len(x)//2], x[len(x)//2:].strip()] for x in f.readlines()]
@@ -17,6 +19,19 @@ def find_priority_sum(rucksacks):
         sum += get_priority(find_common_item(ruck))
     return sum
 
+def find_badge(group):
+    for item in group[0]:
+        if item in group[1]:
+            if item in group[2]:
+                return item
+
+def find_badge_sum(rucksacks):
+    sum = 0
+    for group in chunked([''.join(x) for x in rucksacks], 3):
+        sum += get_priority(find_badge(group))
+    return sum
+
 if __name__ == "__main__":
     rucksacks = read_input("input.txt")
     print(find_priority_sum(rucksacks))
+    print(find_badge_sum(rucksacks))
