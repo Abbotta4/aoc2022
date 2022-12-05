7024b07eabfee4c54231ac8f3faa0af10a96ee2f
Drew Abbott <abbotta4@gmail.com>
2022-12-05 14:50:28 -0600

    Add day 5 part 2

--- day_05/day_05.py
+++ day_05/day_05.py
@@ -10,10 +10,18 @@ def read_input(infile, stacks):
     instructions = [re.findall(r'move (\d+) from (\d) to (\d)', x)[0] for x in instructions.split('\n')]
     return list(map(lambda x: (int(x[0]), int(x[1]) - 1, int(x[2]) - 1), instructions))
 
-def move_crates(stacks, instructions):
+def move_crates_9000(stacks, instructions):
     for num, src, dst in instructions:
         for _ in range(int(num)):
             stacks[dst].append(stacks[src].pop())
+
+def move_crates_9001(stacks, instructions):
+    temp = list()
+    for num, src, dst in instructions:
+        for _ in range(int(num)):
+            temp.append(stacks[src].pop())
+        for _ in range(int(num)):
+            stacks[dst].append(temp.pop())
 
 def get_crate_tops(stacks):
     result = ""
@@ -24,5 +32,9 @@ def get_crate_tops(stacks):
 if __name__ == "__main__":
     stacks = [ [] for _ in range(9)] # 9 stacks of crates
     instructions = read_input("input.txt", stacks)
-    move_crates(stacks, instructions)
+    move_crates_9000(stacks, instructions)
+    print(get_crate_tops(stacks))
+    stacks = [ [] for _ in range(9)]
+    instructions = read_input("input.txt", stacks)
+    move_crates_9001(stacks, instructions)
     print(get_crate_tops(stacks))
