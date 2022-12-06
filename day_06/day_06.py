3636826a862a25363215e622e91b723ff6856821
Drew Abbott <abbotta4@gmail.com>
2022-12-06 14:35:47 -0600

    Add day 6 part 2

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -15,11 +15,12 @@ def sliding_window(iterable, n):
         window.append(x)
         yield tuple(window)
 
-def get_packet_start(signal):
-    for idx, char in enumerate(sliding_window(signal, 4)):
+def get_unique_start(signal, n):
+    for idx, char in enumerate(sliding_window(signal, n)):
         if len(set(char)) == len(char):
-            return idx + 4
+            return idx + n
     
 if __name__ == "__main__":
     signal = read_input("input.txt")
-    print(get_packet_start(signal))
+    print(get_unique_start(signal, 4))
+    print(get_unique_start(signal, 14))
